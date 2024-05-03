c730f8a973e66c10f072cf61bbab80ec469e8b8f
Utkarsh Sahu <136886474+UtkarshSahu9906@users.noreply.github.com>
2024-05-03 12:20:55 +0000

    dataTypes

---
+++ 01_basics/01_ariable.js
@@ -0,0 +1,6 @@
+const accountId = 13349 /// const is a final 
+let accountEmail = "utkarsh@gmail.com" /// let is not final
+var accountPassword = "12346"  /// not recomended to use var in places of let
+accountState ="jhansi"// not recomended to define this type use let
+
+console.table([accountId,accountEmail,accountPassword,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js as newer vertion of Js
+
+//alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3)
+
+console.log("Utkarsh")
+
+
+let name = "Utkarsh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>uniqe
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
