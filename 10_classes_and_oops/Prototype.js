c0b95ee8acdab9b8058958aba8c1a941f6f210fd
Utkarsh Sahu <136886474+UtkarshSahu9906@users.noreply.github.com>
2024-05-09 10:16:46 +0000

    dfdf

---
+++ 10_classes_and_oops/Prototype.js
@@ -0,0 +1,17 @@
+// let myName ="hitesh    "
+
+
+// console.log(myName.trueLenght);
+
+
+
+let myHeros = ["thor","spiderman"]
+
+let heroPower={
+    thor: "hammer",
+    spiderman: "sling"
+
+    getSpiderPower: function(){
+        console.log(`Spidy power id ${this.spiderman}`)
+    }
+}
